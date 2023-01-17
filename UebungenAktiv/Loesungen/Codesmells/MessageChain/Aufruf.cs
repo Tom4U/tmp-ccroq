37f4b8e867fefe7c402dd23153a233cd93049a1e
Thomas Ohms <thomas@ohms.pro>
2023-01-17 14:17:45 +0100

    loesung message

---
+++ UebungenAktiv/Loesungen/Codesmells/MessageChain/Aufruf.cs
@@ -0,0 +1,11 @@
+﻿namespace Loesungen.Codesmells.MessageChain;
+
+internal class Aufruf
+{
+    public static void Main()
+    {
+        Mitglied mitglied = new("Martin", new Verein(new Mitglied("Fred")));
+
+        Console.WriteLine($"Der Vorsitzende ist {mitglied.GetNameVonVorsitzendem()}");
+    }
+}
