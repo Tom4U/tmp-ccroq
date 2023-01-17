79c3d23fee07efa20b1d93aca058b3bfea4a4581
Thomas Ohms <thomas@ohms.pro>
2023-01-17 14:49:23 +0100

    uebung dataclass

---
+++ UebungenAktiv/Challenges/Codesmells/Dataclass/Reservierung.cs
@@ -0,0 +1,17 @@
+﻿namespace Challenges.Codesmells.Dataclass;
+
+internal class Reservierung
+{
+    private DateTime ankunft;
+    private DateTime abreise;
+
+    public Reservierung(DateTime ankunft, DateTime abreise)
+    {
+        this.ankunft = ankunft;
+        this.abreise = abreise;
+    }
+
+    public DateTime GetAnkunft() => ankunft;
+
+    public DateTime GetAbreise() => abreise;
+}
---
+++ UebungenAktiv/Challenges/Codesmells/Dataclass/ReservierungsApp.cs
@@ -0,0 +1,26 @@
+﻿namespace Challenges.Codesmells.Dataclass;
+
+internal class ReservierungsApp
+{
+    public static void Main()
+    {
+        try
+        {
+            DateTime ankunft = DateTime.Parse("20.07.2022");
+            DateTime abreise = DateTime.Parse("23.07.2022");
+            Reservierung reservierung = new Reservierung(ankunft, abreise);
+
+            DauerAnzeigen(reservierung);
+        }
+        catch (FormatException formatException)
+        {
+        }
+    }
+
+    public static void DauerAnzeigen(Reservierung reservierung)
+    {
+        TimeSpan timeDiff = reservierung.GetAbreise() - reservierung.GetAnkunft();
+
+        Console.WriteLine($"Der Gast bleibt { timeDiff.Days } Tage");
+    }
+}
