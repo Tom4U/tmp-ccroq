b481eb52a6900050304520dc2ed05db80cf2e171
Thomas Ohms <thomas@ohms.pro>
2023-01-17 15:18:46 +0100

    loesung dataclass

---
+++ UebungenAktiv/Loesungen/Codesmells/Dataclass/Reservierung.cs
@@ -0,0 +1,24 @@
+﻿namespace Loesungen.Codesmells.Dataclass;
+
+internal class Reservierung
+{
+    private DateTime ankunft;
+    private DateTime abreise;
+
+    public Reservierung(DateTime ankunft, DateTime abreise)
+    {
+        this.ankunft = ankunft;
+        this.abreise = abreise;
+    }
+
+    public void DauerAnzeigen()
+    {
+        TimeSpan timeDiff = abreise - ankunft;
+
+        Console.WriteLine($"Der Gast bleibt {timeDiff.Days} Tage");
+    }
+
+    public DateTime GetAnkunft() => ankunft;
+
+    public DateTime GetAbreise() => abreise;
+}
---
+++ UebungenAktiv/Loesungen/Codesmells/Dataclass/ReservierungsApp.cs
@@ -0,0 +1,19 @@
+﻿namespace Loesungen.Codesmells.Dataclass;
+
+internal class ReservierungsApp
+{
+    public static void Main()
+    {
+        try
+        {
+            DateTime ankunft = DateTime.Parse("20.07.2022");
+            DateTime abreise = DateTime.Parse("23.07.2022");
+            Reservierung reservierung = new Reservierung(ankunft, abreise);
+
+            reservierung.DauerAnzeigen();
+        }
+        catch (FormatException formatException)
+        {
+        }
+    }
+}
