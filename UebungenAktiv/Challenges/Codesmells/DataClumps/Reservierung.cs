13f12cb1eb705d3eaf8e36f906ed4a378b891917
Thomas Ohms <thomas@ohms.pro>
2023-01-17 11:45:47 +0100

    uebung dataclumps

---
+++ UebungenAktiv/Challenges/Codesmells/DataClumps/Reservierung.cs
@@ -0,0 +1,17 @@
+﻿namespace Challenges.Codesmells.DataClumps;
+
+internal class Reservierung
+{
+    private int reservierungsNr;
+    private int zimmerNr;
+    private DateTime ankunft;
+    private DateTime abreise;
+
+    public Reservierung(int reservierungsNr, int zimmerNr, DateTime ankunft, DateTime abreise)
+    {
+        this.reservierungsNr = reservierungsNr;
+        this.zimmerNr = zimmerNr;
+        this.ankunft = ankunft;
+        this.abreise = abreise;
+    }
+}
