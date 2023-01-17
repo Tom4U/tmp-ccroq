f8f7d2f88d0af3307b1e2cee4450568e8c118687
Thomas Ohms <thomas@ohms.pro>
2023-01-17 14:19:44 +0100

    uebung incomplete lib

---
+++ UebungenAktiv/Challenges/Codesmells/IncompleteLibrary/GetraenkeApp.cs
@@ -0,0 +1,32 @@
+﻿using Challenges.Codesmells.IncompleteLibrary.Vendor;
+
+namespace Challenges.Codesmells.IncompleteLibrary;
+
+internal class GetraenkeApp
+{
+    private static Getraenkeautomat getraenkeAutomat = new();
+
+    public static void Main()
+    {
+        foreach (Getraenk getraenk in GetHeissgetraenke())
+        {
+            Console.WriteLine(getraenk.GetName());
+        }
+    }
+
+    public static HashSet<Getraenk> GetHeissgetraenke()
+    {
+        HashSet<Getraenk> alleGetraenke = getraenkeAutomat.GetGetraenke();
+        HashSet<Getraenk> heissGetraenke = new();
+
+        foreach (Getraenk getraenk in alleGetraenke)
+        {
+            if (getraenk.GetKategorie() == "Heißgetränke")
+            {
+                heissGetraenke.Add(getraenk);
+            }
+        }
+
+        return heissGetraenke;
+    }
+}
---
+++ UebungenAktiv/Challenges/Codesmells/IncompleteLibrary/Vendor/Getraenkeautomat.cs
@@ -0,0 +1,18 @@
+﻿namespace Challenges.Codesmells.IncompleteLibrary.Vendor;
+
+internal class Getraenkeautomat
+{
+    private HashSet<Getraenk> getraenke = new();
+
+    public Getraenkeautomat()
+    {
+        getraenke.Add(new Getraenk("Mineralwasser", "Kaltgetränke", new decimal(1.20)));
+        getraenke.Add(new Getraenk("Tafelwasser", "Kaltgetränke", new decimal(1.00)));
+        getraenke.Add(new Getraenk("Orangensaft", "Kaltgetränke", new decimal(2.80)));
+        getraenke.Add(new Getraenk("Kaffee", "Heißgetränke", new decimal(1.30)));
+        getraenke.Add(new Getraenk("Espresso", "Heißgetränke", new decimal(1.50)));
+        getraenke.Add(new Getraenk("Tee", "Heißgetränke", new decimal(1.10)));
+    }
+
+    public HashSet<Getraenk> GetGetraenke() => getraenke;
+}
