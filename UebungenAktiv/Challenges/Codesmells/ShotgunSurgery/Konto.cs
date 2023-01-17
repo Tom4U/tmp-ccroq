0c88082abc3e3632c8252b6f7e74eb7e78af4615
Thomas Ohms <thomas@ohms.pro>
2023-01-17 11:15:36 +0100

    uebung shotgun

---
+++ UebungenAktiv/Challenges/Codesmells/ShotgunSurgery/Konto.cs
@@ -0,0 +1,47 @@
+﻿namespace Challenges.Codesmells.ShotgunSurgery;
+
+public class Konto
+{
+    private string typ;
+    private string kontonummer;
+    private int kontostand;
+
+    public Konto(string typ, string kontonummer, int kontostand)
+    {
+        this.kontostand = kontostand;
+        this.typ = typ;
+        this.kontonummer = kontonummer;
+    }
+
+    public void Abheben(int betrag)
+    {
+        if (kontostand <= 500)
+        {
+            throw new Exception("Kontostand muss über 500 sein");
+        }
+
+        kontostand -= betrag;
+
+        Console.WriteLine($"Kontostand ist {kontostand}");
+
+    }
+
+    public void Umbuchen(Konto sender, Konto empfaenger, int betrag)
+    {
+        if (sender.kontostand <= 500)
+        {
+            throw new Exception("Kontostand muss über 500 sein");
+        }
+
+        empfaenger.kontostand = kontostand + betrag;
+
+    }
+
+    public void WarnungSenden()
+    {
+        if (kontostand <= 500)
+        {
+            Console.WriteLine("Kontostand muss über 500 sein");
+        }
+    }
+}
---
+++ UebungenAktiv/Challenges/Codesmells/ShotgunSurgery/ShotgunSurgery.cs
@@ -0,0 +1,12 @@
+﻿namespace Challenges.Codesmells.ShotgunSurgery;
+
+public class ShotgunSurgery
+{
+    public static void Main()
+    {
+        Konto konto = new("Personal", "KT123456", 1000);
+        konto.Abheben(500);
+        konto.WarnungSenden();
+        konto.Abheben(500);
+    }
+}
