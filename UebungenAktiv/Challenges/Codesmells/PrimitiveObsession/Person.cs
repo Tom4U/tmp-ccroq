87b0bd85d4ec01ae645c41d1c996c7666ecc5dd5
Thomas Ohms <thomas@ohms.pro>
2023-01-17 13:12:00 +0100

    uebung primitive

---
+++ UebungenAktiv/Challenges/Codesmells/PrimitiveObsession/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace Challenges.Codesmells.PrimitiveObsession;
+
+public class Person
+{
+    private string id;
+    private string vorname;
+    private string nachname;
+    private string adresse;
+    private string plz;
+    private string ort;
+    private string land;
+
+    public Person(string id, string vorname, string nachname, string adresse, string plz, string stadt, string land)
+    {
+        // ... Initialisierung ...
+    }
+
+    public void AdresseAendern(string adresse, string plz, string stadt, string land)
+    {
+        // ... Logik ...
+    }
+}
