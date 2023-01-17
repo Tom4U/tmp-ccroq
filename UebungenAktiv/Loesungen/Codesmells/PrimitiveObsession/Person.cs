92494db4bdeb6708203476beefa22d800d28db41
Thomas Ohms <thomas@ohms.pro>
2023-01-17 13:20:23 +0100

    loesung primitives

---
+++ UebungenAktiv/Loesungen/Codesmells/PrimitiveObsession/Person.cs
@@ -0,0 +1,58 @@
+﻿namespace Loesungen.Codesmells.PrimitiveObsession;
+
+public class Person
+{
+    private PersonId id;
+    private Name name;
+    private Adresse adresse;
+
+    public Person(PersonId id, Name name, Adresse adresse)
+    {
+        // ... Initialisierung ...
+    }
+
+    public void AdresseAendern(Adresse adresse)
+    {
+        // ... Logik ...
+    }
+
+    public class Name
+    {
+        string vorname;
+        string nachname;
+
+        public Name(string vorname, string nachname)
+        {
+            this.vorname = vorname;
+            this.nachname = nachname;
+        }
+    }
+
+    public class Adresse
+    {
+        private string adresse;
+        private string plz;
+        private string ort;
+        private string land;
+
+        public Adresse(string adresse, string plz, string stadt, string land)
+        {
+            // ... Initialisierung ...
+        }
+    }
+
+    public class PersonId : IComparable<PersonId>
+    {
+        private string id;
+
+        public PersonId(string id)
+        {
+            // ... Initialisierung ...
+        }
+
+        public int CompareTo(PersonId? other)
+        {
+            return 0;
+        }
+    }
+}
