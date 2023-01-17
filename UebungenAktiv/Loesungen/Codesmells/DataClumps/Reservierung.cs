c7ad8c0c19151234437db6a350b2551a5871eab5
Thomas Ohms <thomas@ohms.pro>
2023-01-17 11:59:03 +0100

    loesung dataclumps

---
+++ UebungenAktiv/Loesungen/Codesmells/DataClumps/Reservierung.cs
@@ -0,0 +1,27 @@
+﻿namespace Loesungen.Codesmells.DataClumps;
+
+internal class Reservierung
+{
+    private int reservierungsNr;
+    private int zimmerNr;
+    private Zeitraum zeitraum;
+
+    public Reservierung(int reservierungsNr, int zimmerNr, Zeitraum zeitraum)
+    {
+        this.reservierungsNr = reservierungsNr;
+        this.zimmerNr = zimmerNr;
+        this.zeitraum = zeitraum;
+    }
+
+    public class Zeitraum
+    {
+        private DateTime ankunft;
+        private DateTime abreise;
+
+        public Zeitraum(DateTime ankunft, DateTime abreise)
+        {
+            this.ankunft = ankunft;
+            this.abreise = abreise;
+        }
+    }
+}
